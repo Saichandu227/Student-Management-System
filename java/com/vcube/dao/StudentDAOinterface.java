02528a4ced503f44c9a02ed3197d19ebd093bd75
Saichandu227 <166286931+Saichandu227@users.noreply.github.com>
2025-03-17 22:18:02 +0530

    StudentDAOinterface.java

--- java/com/vcube/dao/StudentDAOinterface.java
+++ java/com/vcube/dao/StudentDAOinterface.java
@@ -5,5 +5,8 @@ import com.vcube.model.StudentModel;
 public interface StudentDAOinterface {
 
 	public String insertData(StudentModel sd);
+	public StudentModel readData(StudentModel sm);
+	public boolean updateStudent(StudentModel student);
+	public boolean updatePassword(StudentModel student);
 
 }
